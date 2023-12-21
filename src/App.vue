f8e960897a0275c31f02afc8ccc1321d9370e085
AxelSalim <adjakidjememiaghe@gmail.com>
2023-12-21 14:53:02 +0100

    NavBar créé

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import HeaderComp from "@/components/HeaderComp.vue"
+import NavBar from "@/components/NavBar.vue";
 
 </script>
 
 <template>
   <HeaderComp />
-  
+  <NavBar />
   <main>
 
   </main>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import requests from "@/utils/request";
+
+const genreList: string[] = [];
+
+Object.entries(requests).forEach(([key, {title, url}]) => {
+    genreList.push(title);
+})
+
+</script>
+
+<template>
+    <nav>
+        <!-- <ul class="flex px-10 sm:px-20 text-2xl whitespace-nowrap gap-10 sm:gap-20 overflow-x-scroll">
+            <li v-for="request of requests" :key="request">
+                <button>{{ request.title }}</button>
+            </li>
+        </ul> -->
+
+        <ul class="flex px-10 sm:px-20 text-2xl whitespace-nowrap gap-10 sm:gap-20 overflow-x-scroll scrollbar-hide">
+            <li v-for="genre of genreList" :key="genre" class="transition duration-300 transform hover:scale-125 hover:text-white active:text-red-500">
+                <button>{{ genre }}</button>
+            </li>
+
+        </ul>
+    </nav>
+</template>
